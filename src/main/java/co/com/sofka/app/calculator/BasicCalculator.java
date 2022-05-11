ddf42da1bcb3d518c95b2f7df106560d39917927
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-10 19:56:51 -0500

    first commit

---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -0,0 +1,33 @@
+package co.com.sofka.app.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long resta(Long number1, Long number2) {
+        logger.info("Restando {} - {}", number1, number2);
+        return number1 - number2;
+    }
+
+    public Long division(Long number1, Long number2){
+        logger.info("Dividiendo {} / {}", number1, number2);
+        if(number2==0){
+            System.out.println("No se puede dividir por cero");
+        }
+        return number1 / number2;
+    }
+
+    public Long multiplicación(Long number1, Long number2){
+        logger.info("Multiplicando {} * {}", number1, number2);
+        return number1 * number2;
+    }
+}
