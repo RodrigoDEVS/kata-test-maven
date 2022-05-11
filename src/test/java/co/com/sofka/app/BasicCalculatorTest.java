3ba19d2f2c6f8ad6e3335181ff6625ef0ab18c38
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-10 20:17:25 -0500

    Tests Agregados: se agregaron todos los test requeridos para las operaciones creadas

--- src/test/java/co/com/sofka/app/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -39,4 +39,88 @@ public class BasicCalculatorTest {
         assertEquals(expectedResult, basicCalculator.sum(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
+
+    @Test
+    @DisplayName("Testing resta: 1-1=0")
+    public void resta() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 0L;
+
+        // Act
+        Long result = basicCalculator.resta(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several restas")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "1,    1,   0",
+            "2,    1,   1",
+            "100,  49, 51",
+            "100,  1, 99"
+    })
+    public void severalRestas(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.resta(first, second),
+                () -> first + " - " + second + " should equal " + expectedResult);
+    }
+
+    @Test
+    @DisplayName("Testing multiplicacion: 1*1=1")
+    public void multiplicacion() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 1L;
+
+        // Act
+        Long result = basicCalculator.multiplicación(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several multiplicaciones")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "1,    1,   1",
+            "2,    1,   2",
+            "10,  10, 100",
+            "5,  4, 20"
+    })
+    public void severalMultiplicaciones(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.multiplicación(first, second),
+                () -> first + " * " + second + " should equal " + expectedResult);
+    }
+
+    @Test
+    @DisplayName("Testing división: 1/1=1")
+    public void division() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 1L;
+
+        // Act
+        Long result = basicCalculator.division(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several divisiones")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "1,    1,   1",
+            "2,    1,   2",
+            "100,  10, 10",
+            "80,  4, 20"
+    })
+    public void severalDivisiones(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.division(first, second),
+                () -> first + " / " + second + " should equal " + expectedResult);
+    }
 }
